1e2286f7e29079e73d824062d889ed025e935f9d
elonehoo <hi@elonehoo.me>
2025-03-21 18:50:21 +0800

    chore: init first commit

---
+++ docs/.astro/integrations/_astrojs_starlight/i18n-plugins.d.ts
@@ -0,0 +1,8 @@
+declare namespace StarlightApp {
+	type PluginUIStringKeys = {
+		'theme-destyler.home': string;
+		'theme-destyler.links.doc': string;
+		'theme-destyler.links.api': string;
+	};
+	interface I18n extends PluginUIStringKeys {}
+}
---
+++ docs/astro.config.mjs
@@ -0,0 +1,51 @@
+// @ts-check
+import starlight from '@astrojs/starlight'
+import { defineConfig } from 'astro/config'
+import destylerTheme from 'starlight-theme-destyler'
+
+export default defineConfig({
+  integrations: [
+    starlight({
+      logo: {
+        dark: './src/assets/logo-dark.svg',
+        light: './src/assets/logo-light.svg',
+        alt: 'Starlight Black',
+      },
+      editLink: {
+        baseUrl: 'https://github.com/adrian-ub/starlight-theme-destyler/edit/main/docs/',
+      },
+      plugins: [
+        destylerTheme({
+          navLinks: [
+            {
+              label: 'Docs',
+              link: '/getting-started',
+            },
+            {
+              label: 'Starlight',
+              link: 'https://starlight.astro.build',
+              badge: 'External',
+              attrs: {
+                target: '_blank',
+                rel: 'noopener',
+              },
+            },
+          ],
+        }),
+      ],
+      sidebar: [
+        {
+          label: 'Start Here',
+          items: [
+            { slug: 'getting-started' },
+            { slug: 'test-toc', badge: 'New' },
+          ],
+        },
+      ],
+      social: {
+        github: 'https://github.com/adrian-ub/starlight-theme-destyler',
+      },
+      title: '@destyler/theme',
+    }),
+  ],
+})
